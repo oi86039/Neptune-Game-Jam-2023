f2533e0fbb83c503643bf990fc372dd76c00acc8
John Campbell <70589807+jfc33@users.noreply.github.com>
2023-02-04 20:23:42 -0500

    Add files via upload

--- Scripts/PlayerMovement.cs
+++ Scripts/PlayerMovement.cs
@@ -18,10 +18,15 @@ public class PlayerMovement : MonoBehaviour
     float gravity;
     bool crouching = false;
     bool sprinting = false;
+    public bool facingF = true;
     Vector3 velocity = new Vector3(0, 0, 0);
-
+    public Vector2 input = new Vector2();
     ControllerHandler controller;
     Transform sprite;
+    public Object prefab;
+    public float flashCool = 4;
+    bool canFlash = true;
+    float flashCoolTotal;
 
     void Start()
     {
@@ -29,11 +34,30 @@ public class PlayerMovement : MonoBehaviour
         sprite = transform.Find("Sprite");
         jumpForce = (2f * jumpHeight) / (jumpTime / 2f);
         gravity = (-2f * jumpHeight) / Mathf.Pow(jumpTime / 2f, 2f);
+        flashCoolTotal = flashCool;
     }
 
     void Update()
     {
-        
+        if(canFlash == false)
+        {
+            flashCool -= Time.deltaTime;
+            Debug.Log("FALSE");
+        }
+        if(flashCool < 0)
+        {
+            canFlash = true;
+            flashCool = flashCoolTotal;
+        }
+        if (!crouching)
+        {
+            if (Input.GetKey(KeyCode.Mouse0) && canFlash)
+            {
+                Instantiate(prefab, sprite.transform.rotation.y == 0 ? transform.position + Vector3.right * 5 : transform.position + Vector3.right * -5, Quaternion.identity);
+                canFlash = false;
+                Debug.Log("FLASHING");
+            }
+        }
     }
     private void FixedUpdate()
     {
@@ -41,25 +65,27 @@ public class PlayerMovement : MonoBehaviour
         {
             velocity.y = 0;
         }
-        Vector2 input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
         if (input.y < 0 && controller.collisions.below)
         {
-            //change sprite
+            //duck animation trigger
             transform.localScale = new Vector3(0.85f, 0.75f, 1);
             if (!crouching)
             {
                 transform.Translate(Vector3.down * 0.225f);
-                breakSpeed /= 2 * sprintModifier;
+                breakSpeed /= sprintModifier;
             }
+            input.x = 0;
             crouching = true;
         }
         else
         {
+            //stand animation trigger
             transform.localScale = new Vector3(0.7f, 1.2f, 1);
             if (crouching)
             {
                 transform.Translate(Vector3.up * 0.225f);
-                breakSpeed *= sprintModifier * 2;
+                breakSpeed *= sprintModifier;
             }
             crouching = false;
         }
@@ -80,28 +106,30 @@ public class PlayerMovement : MonoBehaviour
             }
             sprinting = false;
         }
-        if (input.x == 0)
-        {
-            float step = (velocity.x - 0) / breakSpeed;
-            if (step < 0.0001 && step > -0.0001)
+            if (input.x == 0)
             {
-                velocity.x = 0;
+                float step = (velocity.x - 0) / breakSpeed;
+                if (step < 0.0001 && step > -0.0001)
+                {
+                    velocity.x = 0;
+                }
+                else
+                {
+                    velocity.x -= step;
+                }
             }
-            else
-            {
-                velocity.x -= step;
-            }
-        }
-        else if (!crouching)
+        if (!crouching)
         {
             velocity.x = Mathf.Clamp(velocity.x + input.x * moveSpeed * Time.deltaTime, -maxSpeed, maxSpeed);
             if (velocity.x > 0f)
             {
                 sprite.transform.eulerAngles = Vector3.zero;
+                facingF = true;
             }
             else if (velocity.x < 0f)
             {
                 sprite.transform.eulerAngles = new Vector3(0f, 180f, 0f);
+                facingF = false;
             }
         }
         if ((Input.GetKey(KeyCode.Space) || input.y > 0) && controller.collisions.below && !crouching)
